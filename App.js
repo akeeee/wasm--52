532aa792ab969e25814134e7f281aa5018f1fcea
akeeee <ake@nuxos.asia>
2024-11-21 14:20:39 +0700

    Add WASM

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import init, { greet } from "./pkg/hello_wasm"
+import { useEffect } from 'react';
 export default function App() {
+  useEffect(async () => {
+    await init().then(() => console.log('GREET', greet()))
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
